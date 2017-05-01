9fc49a0013023d95ad6cd712ddae673fb73e7c76
Sukruth M K <suk_mallur@yahoo.co.in>
2017-05-01 17:37:26 -0500

    testing out the ui

---
+++ js/popup.js
@@ -0,0 +1,45 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     var checkPageButton = document.getElementById('checkPage');
+//     checkPageButton.addEventListener('click', function() {
+//         chrome.tabs.getSelected(null, function(tab) {
+//             d = document;
+//
+//             var f = d.createElement('form');
+//             f.action = 'http://gtmetrix.com/analyze.html?bm';
+//             f.method = 'post';
+//             var i = d.createElement('input');
+//             i.type = 'hidden';
+//             i.name = 'url';
+//             i.value = tab.url;
+//             f.appendChild(i);
+//             d.body.appendChild(f);
+//             f.submit();
+//         });
+//     });
+// });
+angular.module('toolbarDemo2', ['ngMaterial'])
+
+.config(function($mdIconProvider) {
+    $mdIconProvider
+         .icon('search', 'assets/icons/ic_search_black_24px.svg')
+         .icon('favorite', 'assets/icons/ic_favorite_black_24px.svg')
+         .icon('history', 'assets/icons/ic_history_black_24px.svg')
+})
+
+.controller('TitleController', function($scope) {
+    console.log("coming inside");
+  $scope.title = 'My App Title';
+})
+.controller('AppCtrl', function($scope) {
+  var imagePath = 'img/list/60.jpeg';
+
+  $scope.todos = [];
+  for (var i = 0; i < 15; i++) {
+    $scope.todos.push({
+      face: imagePath,
+      what: "Brunch this weekend?",
+      who: "Min Li Chan",
+      notes: "I'll be in your neighborhood doing errands."
+    });
+  }
+});
